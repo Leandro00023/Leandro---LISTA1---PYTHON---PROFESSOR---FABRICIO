4d626746eef0e762fad12bce4f40e7a53a11b7ba
Leandro Henrique <80431106+Leandro00023@users.noreply.github.com>
2021-03-13 12:04:32 -0300

    Exercício 1 da 1º lista

---
+++ Exercicio 4.py
@@ -0,0 +1,11 @@
+#4.	Crie um algoritmo para imprimir a sequência dos 100 primeiros números pares, usando o For e usando o While.
+
+for c in range(1, 200, 2):
+    print(c+1)
+print ('FIM do comando for')
+
+c = 2
+while c <= 200:
+    print(c)
+    c = c + 2
+print('FIM do comando while')

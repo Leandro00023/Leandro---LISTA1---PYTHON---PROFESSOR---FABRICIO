d57efd8137fc38fbf4fa5c242ac32ad2f3bc0c10
Leandro Henrique <80431106+Leandro00023@users.noreply.github.com>
2021-03-14 13:20:25 -0300

    Add files via upload
    Exercício 3 da 1º lista

---
+++ Exercicio 3.py
@@ -0,0 +1,16 @@
+#3.	Crie um algoritmo para exibir a sequência de Fibonacci para um número informado pelo usuário.
+# Por exemplo, se ele informar 5, deve-se imprimir até o quinto número da sequência.
+import math
+
+f1 = 0
+f2 = 1
+f = int(input('Digite um numero: '))
+print ('{} {}'.format (f1, f2))
+while f != 6:
+    print (f)
+    f += 1
+
+#MINHA MENTE FALHOU NESSA
+
+
+

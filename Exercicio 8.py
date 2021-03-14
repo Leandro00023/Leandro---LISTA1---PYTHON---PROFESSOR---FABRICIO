39a2e6ef180cff52a186388c14b14d75ff64d50f
Leandro Henrique <80431106+Leandro00023@users.noreply.github.com>
2021-03-14 13:20:46 -0300

    Add files via upload
    Exercício 8 da 1º lista

---
+++ Exercicio 8.py
@@ -0,0 +1,9 @@
+#8. Escreva um programa em Python para exibir apenas o primeiro e o último item deuma lista de cores.
+#color_list = [“Red”, “Green”, “White” ,“Black”]
+
+color_list = ['Red', 'Green', 'Blue', 'yellow','White','Black']
+print (color_list[0])
+print (len(color_list))
+print (color_list[len(color_list)-1])
+
+

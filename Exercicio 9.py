a835ed657ce70095e6dbabcf76c56bb362eaf523
Leandro Henrique <80431106+Leandro00023@users.noreply.github.com>
2021-03-14 13:21:02 -0300

    Add files via upload
    Exercício 9 da 1º lista

---
+++ Exercicio 9.py
@@ -0,0 +1,12 @@
+# Crie um programa em Python que receba um inteiro (n) e calcule o valor de n + nn + nnn O valor dado de n é 5 O resultado esperado: 615
+
+n = str(input('Digite um numero: '))
+n = int(n)+int(n+n)+int(n+n+n)
+print (n)
+
+
+
+
+
+
+
